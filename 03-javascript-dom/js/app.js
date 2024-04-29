ef955fdf48d6b65af3a4291efdc9562c8b5e4725
Sumit Khandelwal <synergy2411@outlook.com>
2024-04-29 17:16:04 +0530

    fetch api

--- 03-javascript-dom/js/app.js
+++ 03-javascript-dom/js/app.js
@@ -2,6 +2,8 @@ window.onload = function () {
   const inputTitle = document.getElementById("title");
   const btnAdd = document.getElementById("btn-add");
   const listContainer = document.querySelector("#list-container");
+  const headingEl = document.querySelector("#heading");
+  const btnFetchTodos = document.getElementById("btn-fetch-todos");
 
   btnAdd.addEventListener("click", function (event) {
     event.preventDefault();
@@ -13,4 +15,30 @@ window.onload = function () {
     listContainer.appendChild(liElement);
     inputTitle.value = "";
   });
+
+  btnFetchTodos.addEventListener("click", function () {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (result) {
+        result.forEach(function (todo) {
+          const liElement = document.createElement("li");
+          liElement.innerHTML = todo.title;
+          listContainer.appendChild(liElement);
+        });
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  });
+
+  //   setTimeout(() => {
+  //     alert("After two seconds");
+  //   }, 2000);
+
+  headingEl.style.color = "red";
 };
+
+// then().catch()
+// Async...await function
