9b51e52ab20eba9fe7ae4ef7894a4671492f27d4
Sumit Khandelwal <synergy2411@outlook.com>
2024-04-30 15:12:08 +0530

    jsx code

--- frontend/src/Components/Expenses/Expenses.jsx
+++ frontend/src/Components/Expenses/Expenses.jsx
@@ -1,5 +1,34 @@
+import React from "react";
+
 function Expenses() {
-  return <h2>Expenses coming soon...</h2>;
+  let product = {
+    name: "Apple iPhone",
+    quantity: 199,
+    description: "This is awesome iPhone",
+  };
+
+  let friends = ["Monica", "Joe", "Ross", "Rachel", "Chandler"];
+
+  return (
+    <div>
+      <h2>Expenses coming soon...</h2>
+      <p>Name : {product.name}</p>
+      <p>Quantity : {product.quantity}</p>
+      <p>
+        {product.quantity > 0
+          ? "Product is available"
+          : "Product is unavailable"}
+      </p>
+      <p>{product.description}</p>
+      <h2>Friends</h2>
+      <hr />
+      <ul>
+        {friends.map((friend) => (
+          <li>{friend}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Expenses;
