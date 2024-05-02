22f489a0d4224806ad215085e40ef5a5eb815958
Sumit Khandelwal <synergy2411@outlook.com>
2024-05-02 17:04:40 +0530

    password validation added

--- frontend/src/Components/Demo/ControlledComp.jsx
+++ frontend/src/Components/Demo/ControlledComp.jsx
@@ -4,8 +4,16 @@ function ControlledComp() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [emailBlurred, setEmailBlurred] = useState(false);
 
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [passwordBlurred, setPasswordBlurred] = useState(false);
+
   const [selectedGender, setSelectedGender] = useState("male");
   const [checkedTnc, setCheckedTnc] = useState(false);
+
+  const passwordChangeHandler = (event) => {
+    setPasswordBlurred(false);
+    setEnteredPassword(event.target.value);
+  };
 
   const emailChangeHandler = (event) => {
     setEmailBlurred(false);
@@ -14,92 +22,123 @@ function ControlledComp() {
 
   const emailIsInvalid = emailBlurred && !enteredEmail.includes("@");
 
+  const passwordIsInvalid = passwordBlurred && enteredPassword.length < 6;
+
   const loginHandler = () => {
     console.log("Email : ", enteredEmail);
+    console.log("Password : ", enteredPassword);
     console.log("Gender : ", selectedGender);
     console.log("Is TnC Checked : ", checkedTnc);
   };
 
   return (
-    <div className="container">
-      <form>
-        {/* Controlled Component - Input Field */}
-        <div className="form-floating mb-3">
-          <input
-            type="email"
-            className="form-control"
-            name="email"
-            id="email"
-            placeholder=""
-            value={enteredEmail}
-            onChange={emailChangeHandler}
-            onBlur={() => setEmailBlurred(true)}
-          />
-          <label htmlFor="email">Email</label>
-          {emailIsInvalid && (
-            <p className="alert alert-danger">Email is not in proper format</p>
-          )}
+    <div className="backdrop">
+      <div className="my-modal">
+        <div className="container">
+          <h1 className="text-center">Auth Form</h1>
+          <form>
+            {/* Controlled Component - Email Input Field */}
+            <div className="form-floating mb-3">
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                id="email"
+                placeholder=""
+                value={enteredEmail}
+                onChange={emailChangeHandler}
+                onBlur={() => setEmailBlurred(true)}
+              />
+              <label htmlFor="email">Email</label>
+              {emailIsInvalid && (
+                <p className="alert alert-danger">
+                  Email is not in proper format
+                </p>
+              )}
+            </div>
+
+            {/* Controlled Component - Password Input Field */}
+
+            <div className="form-floating mb-3">
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                id="password"
+                placeholder=""
+                value={enteredPassword}
+                onChange={passwordChangeHandler}
+                onBlur={() => setPasswordBlurred(true)}
+              />
+              <label htmlFor="password">Password</label>
+              {passwordIsInvalid && (
+                <p className="alert alert-danger">
+                  Password should have at least 6 characters
+                </p>
+              )}
+            </div>
+
+            {/* Controlled Component - Radio Button */}
+            <div className="mb-4">
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="gender"
+                  id="male"
+                  checked={selectedGender === "male"}
+                  value="male"
+                  onChange={() => setSelectedGender("male")}
+                />
+                <label className="form-check-label" htmlFor="gender">
+                  Male
+                </label>
+              </div>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="gender"
+                  id="female"
+                  checked={selectedGender === "female"}
+                  value="female"
+                  onChange={() => setSelectedGender("female")}
+                />
+                <label className="form-check-label" htmlFor="female">
+                  Female
+                </label>
+              </div>
+            </div>
+
+            {/* Controlled Component - Checkbox */}
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="tnc"
+                id="tnc"
+                onChange={(event) => setCheckedTnc(event.target.checked)}
+              />
+              <label className="form-check-label" htmlFor="tnc">
+                {" "}
+                Accept Terms and Condictions{" "}
+              </label>
+            </div>
+
+            {/* Unconntrolled Component */}
+
+            {/* button */}
+            <button
+              className="btn btn-primary"
+              type="button"
+              onClick={loginHandler}
+              disabled={!checkedTnc}
+            >
+              Login
+            </button>
+          </form>
         </div>
-
-        {/* Controlled Component - Radio Button */}
-        <div className="mb-4">
-          <div className="form-check">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="gender"
-              id="male"
-              checked={selectedGender === "male"}
-              value="male"
-              onChange={() => setSelectedGender("male")}
-            />
-            <label className="form-check-label" htmlFor="gender">
-              Male
-            </label>
-          </div>
-          <div className="form-check">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="gender"
-              id="female"
-              checked={selectedGender === "female"}
-              value="female"
-              onChange={() => setSelectedGender("female")}
-            />
-            <label className="form-check-label" htmlFor="female">
-              Female
-            </label>
-          </div>
-        </div>
-
-        {/* Controlled Component - Checkbox */}
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value="tnc"
-            id="tnc"
-            onChange={(event) => setCheckedTnc(event.target.checked)}
-          />
-          <label className="form-check-label" htmlFor="tnc">
-            {" "}
-            Accept Terms and Condictions{" "}
-          </label>
-        </div>
-
-        {/* Unconntrolled Component */}
-
-        {/* button */}
-        <button
-          className="btn btn-primary"
-          type="button"
-          onClick={loginHandler}
-          disabled={!checkedTnc}
-        >
-          Login
-        </button>
-      </form>
+      </div>
     </div>
   );
 }
