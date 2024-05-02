aa2122998ae5e6e9eeacb9a3472b74c6302573d7
Sumit Khandelwal <synergy2411@outlook.com>
2024-05-02 16:42:23 +0530

    email validation added

--- frontend/src/Components/Demo/ControlledComp.jsx
+++ frontend/src/Components/Demo/ControlledComp.jsx
@@ -2,18 +2,28 @@ import { useState } from "react";
 
 function ControlledComp() {
   const [enteredEmail, setEnteredEmail] = useState("");
-  const [selectedGender, setSelectedGender] = useState("male");
+  const [emailBlurred, setEmailBlurred] = useState(false);
 
-  const emailChangeHandler = (event) => setEnteredEmail(event.target.value);
+  const [selectedGender, setSelectedGender] = useState("male");
+  const [checkedTnc, setCheckedTnc] = useState(false);
+
+  const emailChangeHandler = (event) => {
+    setEmailBlurred(false);
+    setEnteredEmail(event.target.value);
+  };
+
+  const emailIsInvalid = emailBlurred && !enteredEmail.includes("@");
 
   const loginHandler = () => {
     console.log("Email : ", enteredEmail);
+    console.log("Gender : ", selectedGender);
+    console.log("Is TnC Checked : ", checkedTnc);
   };
 
   return (
     <div className="container">
       <form>
-        {/* Controlled Component */}
+        {/* Controlled Component - Input Field */}
         <div className="form-floating mb-3">
           <input
             type="email"
@@ -23,37 +33,58 @@ function ControlledComp() {
             placeholder=""
             value={enteredEmail}
             onChange={emailChangeHandler}
+            onBlur={() => setEmailBlurred(true)}
           />
           <label htmlFor="email">Email</label>
+          {emailIsInvalid && (
+            <p className="alert alert-danger">Email is not in proper format</p>
+          )}
         </div>
 
         {/* Controlled Component - Radio Button */}
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="gender"
-            id="male"
-            checked={selectedGender === "male"}
-            value="male"
-            onChange={() => setSelectedGender("male")}
-          />
-          <label className="form-check-label" htmlFor="gender">
-            Male
-          </label>
+        <div className="mb-4">
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="gender"
+              id="male"
+              checked={selectedGender === "male"}
+              value="male"
+              onChange={() => setSelectedGender("male")}
+            />
+            <label className="form-check-label" htmlFor="gender">
+              Male
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="gender"
+              id="female"
+              checked={selectedGender === "female"}
+              value="female"
+              onChange={() => setSelectedGender("female")}
+            />
+            <label className="form-check-label" htmlFor="female">
+              Female
+            </label>
+          </div>
         </div>
+
+        {/* Controlled Component - Checkbox */}
         <div className="form-check">
           <input
             className="form-check-input"
-            type="radio"
-            name="gender"
-            id="female"
-            checked={selectedGender === "female"}
-            value="female"
-            onChange={() => setSelectedGender("female")}
+            type="checkbox"
+            value="tnc"
+            id="tnc"
+            onChange={(event) => setCheckedTnc(event.target.checked)}
           />
-          <label className="form-check-label" htmlFor="female">
-            Female
+          <label className="form-check-label" htmlFor="tnc">
+            {" "}
+            Accept Terms and Condictions{" "}
           </label>
         </div>
 
@@ -64,6 +95,7 @@ function ControlledComp() {
           className="btn btn-primary"
           type="button"
           onClick={loginHandler}
+          disabled={!checkedTnc}
         >
           Login
         </button>
