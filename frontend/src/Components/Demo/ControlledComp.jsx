84cede81b66d8beee250949e979dfc125d1ce548
Sumit Khandelwal <synergy2411@outlook.com>
2024-05-02 16:03:27 +0530

    controlled radio button

--- frontend/src/Components/Demo/ControlledComp.jsx
+++ frontend/src/Components/Demo/ControlledComp.jsx
@@ -1,12 +1,74 @@
-import Input from "../UI/Input";
+import { useState } from "react";
 
 function ControlledComp() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [selectedGender, setSelectedGender] = useState("male");
+
+  const emailChangeHandler = (event) => setEnteredEmail(event.target.value);
+
+  const loginHandler = () => {
+    console.log("Email : ", enteredEmail);
+  };
+
   return (
-    <>
-      <h1>Form Component</h1>
-      <label htmlFor="email">Email : </label>
-      <Input id="email" />
-    </>
+    <div className="container">
+      <form>
+        {/* Controlled Component */}
+        <div className="form-floating mb-3">
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            id="email"
+            placeholder=""
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+          />
+          <label htmlFor="email">Email</label>
+        </div>
+
+        {/* Controlled Component - Radio Button */}
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="gender"
+            id="male"
+            checked={selectedGender === "male"}
+            value="male"
+            onChange={() => setSelectedGender("male")}
+          />
+          <label className="form-check-label" htmlFor="gender">
+            Male
+          </label>
+        </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="gender"
+            id="female"
+            checked={selectedGender === "female"}
+            value="female"
+            onChange={() => setSelectedGender("female")}
+          />
+          <label className="form-check-label" htmlFor="female">
+            Female
+          </label>
+        </div>
+
+        {/* Unconntrolled Component */}
+
+        {/* button */}
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={loginHandler}
+        >
+          Login
+        </button>
+      </form>
+    </div>
   );
 }
 
--- frontend/src/Components/UI/Input.jsx
+++ frontend/src/Components/UI/Input.jsx
@@ -1,16 +1,29 @@
 import classes from "./Input.module.css";
+import { useState } from "react";
 
+// Controlled Component
 function Input(props) {
-  const inputStyle = {
-    backgroundColor: "lightgrey",
-    borderRadius: "5px",
-    padding: "12px",
-    color: "#222",
-  };
+  const [inputValue, setInputValue] = useState("");
+
+  const inputChangeHandler = (event) => setInputValue(event.target.value);
+
+  //   const inputStyle = {
+  //     backgroundColor: "lightgrey",
+  //     borderRadius: "5px",
+  //     padding: "12px",
+  //     color: "#222",
+  //   };
 
   //   return <input style={inputStyle} />;
 
-  return <input className={classes["input-style"]} {...props} />;
+  return (
+    <input
+      className={classes["input-style"]}
+      {...props}
+      value={inputValue}
+      onChange={inputChangeHandler}
+    />
+  );
 }
 
 export default Input;
