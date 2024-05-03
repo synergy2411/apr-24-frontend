6fc3cb0606154a6b09c46dec0030fecb20bede8e
Sumit Khandelwal <synergy2411@outlook.com>
2024-05-03 12:01:12 +0530

    user logged out

--- frontend/src/Components/Demo/ContextDemo/CompA.jsx
+++ frontend/src/Components/Demo/ContextDemo/CompA.jsx
@@ -1,11 +1,16 @@
 import CompB from "./CompB";
 import AuthContext from "../../../context/auth-context";
+import { useState } from "react";
 
 function CompA() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <>
       <h1>Component A</h1>
-      <AuthContext.Provider value={{ isLoggedIn: true }}>
+      <AuthContext.Provider
+        value={{ isLoggedIn: isLoggedIn, setIsLoggedIn: setIsLoggedIn }}
+      >
         <CompB />
       </AuthContext.Provider>
     </>
--- frontend/src/Components/Demo/ContextDemo/CompC.jsx
+++ frontend/src/Components/Demo/ContextDemo/CompC.jsx
@@ -1,21 +1,34 @@
+import { useContext } from "react";
 import AuthContext from "../../../context/auth-context";
 
 function CompC() {
+  const context = useContext(AuthContext);
+
   return (
     <>
-      <AuthContext.Consumer>
-        {(context) => {
-          return (
-            <>
-              <h3>Component C</h3>
-              {context.isLoggedIn && <p>User is Logged in.</p>}
-              {!context.isLoggedIn && <p>User is NOT Logged in</p>}
-            </>
-          );
-        }}
-      </AuthContext.Consumer>
+      <h3>Component C</h3>
+      <button
+        className="btn btn-primary"
+        onClick={() => context.setIsLoggedIn(!context.isLoggedIn)}
+      >
+        Login / Logout
+      </button>
+      {context.isLoggedIn && <p>User is Logged in.</p>}
+      {!context.isLoggedIn && <p>User is NOT Logged in</p>}
     </>
   );
 }
 
 export default CompC;
+
+/* <AuthContext.Consumer>
+{(context) => {
+  return (
+    <>
+      <h3>Component C</h3>
+      {context.isLoggedIn && <p>User is Logged in.</p>}
+      {!context.isLoggedIn && <p>User is NOT Logged in</p>}
+    </>
+  );
+}}
+</AuthContext.Consumer> */
