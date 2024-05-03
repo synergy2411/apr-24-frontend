126571dbed0789380f996b56bbac386881e9e757
Sumit Khandelwal <synergy2411@outlook.com>
2024-05-03 12:58:21 +0530

    counter un reducer

--- frontend/src/Components/Demo/UseReducerDemo.jsx
+++ frontend/src/Components/Demo/UseReducerDemo.jsx
@@ -1,6 +1,24 @@
 import { useReducer } from "react";
 
 function reducerFn(state, action) {
+  if (action.type === "INCREMENT") {
+    // return state.counter++;     // NEVER EVER CHANGE EXISTING STATE
+    return {
+      counter: state.counter + 1,
+    };
+  } else if (action.type === "DECREMENT") {
+    return {
+      counter: state.counter - 1,
+    };
+  } else if (action.type === "ADD_COUNTER") {
+    return {
+      counter: state.counter + action.payload,
+    };
+  } else if (action.type === "SUBTRACT_COUNTER") {
+    return {
+      counter: state.counter - action.payload,
+    };
+  }
   return state;
 }
 
@@ -11,6 +29,31 @@ function UseReducerDemo() {
     <div className="container">
       <h1>Use Reducer Demo Hook</h1>
       <p className="display-4">Counter : {state.counter}</p>
+      <button
+        className="btn btn-primary"
+        onClick={() => dispatch({ type: "INCREMENT" })}
+      >
+        Increase
+      </button>
+      <button
+        className="btn btn-success"
+        onClick={() => dispatch({ type: "DECREMENT" })}
+      >
+        Decrease
+      </button>
+      <button
+        className="btn btn-secondary"
+        onClick={() => dispatch({ type: "ADD_COUNTER", payload: 10 })}
+      >
+        Add - 10
+      </button>
+
+      <button
+        className="btn btn-warning"
+        onClick={() => dispatch({ type: "SUBTRACT_COUNTER", payload: 5 })}
+      >
+        Subtract - 5
+      </button>
     </div>
   );
 }
