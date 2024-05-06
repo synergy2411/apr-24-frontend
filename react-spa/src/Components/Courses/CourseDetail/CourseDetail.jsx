da63804055f864c28ac6dead680189b9e9c29ca1
Sumit Khandelwal <synergy2411@outlook.com>
2024-05-06 12:00:05 +0530

    edit and detete buttons added

--- react-spa/src/Components/Courses/CourseDetail/CourseDetail.jsx
+++ react-spa/src/Components/Courses/CourseDetail/CourseDetail.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import classes from "./CourseDetail.module.css";
 
 /* eslint-disable react/prop-types */
@@ -11,7 +12,24 @@ function CourseDetail({ course }) {
             <div className="card-title">
               <h3>{course.title.toUpperCase()}</h3>
             </div>
-            <div className="card-text">Duration : {course.duration}Hrs</div>
+            <div className="card-text mb-3">
+              Duration : {course.duration}Hrs
+            </div>
+            <Link to="/courses" className="btn btn-secondary mb-4">
+              Close
+            </Link>
+            <div className="row">
+              <div className="col-6">
+                <div className="d-grid">
+                  <button className="btn btn-success">Edit</button>
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="d-grid">
+                  <button className="btn btn-outline-danger">Delete</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
