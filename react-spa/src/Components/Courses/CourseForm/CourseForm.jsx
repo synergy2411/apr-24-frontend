a0a974b39176ca4a3f9fd8a05ad6f4ff69cb71cc
Sumit Khandelwal <synergy2411@outlook.com>
2024-05-06 12:18:29 +0530

    form created

--- react-spa/src/Components/Courses/CourseForm/CourseForm.jsx
+++ react-spa/src/Components/Courses/CourseForm/CourseForm.jsx
@@ -1,5 +1,63 @@
 function CourseForm() {
-  return <>Form coming soon...</>;
+  return (
+    <div className="row">
+      <div className="col-8 offset-2">
+        <h1 className="text-center">Add New Course</h1>
+        <form>
+          {/* title */}
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              name="title"
+              id="title"
+              placeholder=""
+            />
+            <label htmlFor="title">Course Title</label>
+          </div>
+
+          {/* duration */}
+          <div className="form-floating mb-3">
+            <input
+              type="number"
+              className="form-control"
+              name="duration"
+              id="duration"
+              placeholder=""
+            />
+            <label htmlFor="duration">Course Duration</label>
+          </div>
+
+          {/* logo */}
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              name="logo"
+              id="logo"
+              placeholder=""
+            />
+            <label htmlFor="logo">Course Logo</label>
+          </div>
+
+          {/* buttons */}
+
+          <div className="row">
+            <div className="col-6">
+              <div className="d-grid">
+                <button className="btn btn-success">Add</button>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="d-grid">
+                <button className="btn btn-outline-dark">Close</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default CourseForm;
