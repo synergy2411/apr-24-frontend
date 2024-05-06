4fd8b4aab2d52b29440791189ec67343f6c4ee70
Sumit Khandelwal <synergy2411@outlook.com>
2024-05-06 10:51:46 +0530

    course item css added

--- react-spa/src/Components/Courses/CourseItem/CourseItem.jsx
+++ react-spa/src/Components/Courses/CourseItem/CourseItem.jsx
@@ -1,11 +1,15 @@
 import { useNavigate } from "react-router-dom";
+import classes from "./CourseItem.module.css";
 
 /* eslint-disable react/prop-types */
 function CourseItem({ course }) {
   const navigate = useNavigate();
   return (
     <div className="col-4">
-      <div className="card" onClick={() => navigate(`/courses/${course.id}`)}>
+      <div
+        className={`card ${classes["clickable"]}`}
+        onClick={() => navigate(`/courses/${course.id}`)}
+      >
         <div className="card-header">
           <h5 className="text-center">{course.title}</h5>
         </div>
