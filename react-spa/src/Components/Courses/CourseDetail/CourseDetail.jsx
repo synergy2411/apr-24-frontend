de2f536dcbdda76be78ce4a2b727615bee509151
Sumit Khandelwal <synergy2411@outlook.com>
2024-05-06 11:50:44 +0530

    backdrop effect added

--- react-spa/src/Components/Courses/CourseDetail/CourseDetail.jsx
+++ react-spa/src/Components/Courses/CourseDetail/CourseDetail.jsx
@@ -1,10 +1,21 @@
+import classes from "./CourseDetail.module.css";
+
 /* eslint-disable react/prop-types */
 function CourseDetail({ course }) {
-  console.log(course);
   return (
-    <>
-      <h1>Course Detail Component Loaded</h1>
-    </>
+    <div className={classes["backdrop"]}>
+      <div className={classes["my-modal"]}>
+        <div className="card">
+          <img src={course.logo} className="card=img-top" alt={course.title} />
+          <div className="card-body text-center">
+            <div className="card-title">
+              <h3>{course.title.toUpperCase()}</h3>
+            </div>
+            <div className="card-text">Duration : {course.duration}Hrs</div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
