a1020c0b269a6d33838c7af8bff732c04ca44643
Sumit Khandelwal <synergy2411@outlook.com>
2024-05-08 10:58:10 +0530

    tests added

--- react-testing-app/src/Components/DemoOutput/DemoOutput.jsx
+++ react-testing-app/src/Components/DemoOutput/DemoOutput.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 function DemoOutput() {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <>
       <h1>DemoOutput component loaded</h1>
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+
+      {toggle && <p>Toggle is True</p>}
+      {!toggle && <p>Toggle is False</p>}
     </>
   );
 }
--- react-testing-app/src/Components/DemoOutput/DemoOutput.test.js
+++ react-testing-app/src/Components/DemoOutput/DemoOutput.test.js
@@ -1,11 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import DemoOutput from "./DemoOutput";
 
-test("renders heading element with 'DemoOutput component loaded' text", () => {
-  // Arrange
-  render(<DemoOutput />);
-  const headingEl = screen.getByText("DemoOutput component loaded");
+describe("DemoOutput Component", () => {
+  test("renders heading element with 'DemoOutput component loaded' text", () => {
+    // Arrange
+    render(<DemoOutput />);
+    const headingEl = screen.getByText("DemoOutput component loaded");
 
-  // Assert
-  expect(headingEl).toBeInTheDocument();
+    // Assert
+    expect(headingEl).toBeInTheDocument();
+  });
+
+  test("renders paragraph with 'Toggle is False' when button is NOT clicked", () => {
+    render(<DemoOutput />);
+
+    // const paragraphEl = screen.queryByText("toggle is false", { exact: false });
+    const paragraphEl = screen.queryByText(/toggle is false/i);
+
+    expect(paragraphEl).not.toBeNull();
+  });
 });
